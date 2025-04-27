4d25843930edc5c826f21e3ef0e1388edc8df8cd
SebastianJosueBordaAstoray <bordasebastian837@gmail.com>
2025-04-27 17:44:32 -0500

    trabajo2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TAREA_2_PROGRA.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
